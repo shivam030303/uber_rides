1c5c66c51e7ae40bc7e06fac00e7d47dbdfb7519
shubham11104 <72160460+shubham11104@users.noreply.github.com>
2022-10-09 23:18:07 +0530

    Create OpenCamera.js
    Camera functionality added

---
+++ OpenCamera.js
@@ -0,0 +1,28 @@
+// App.js
+cameraLaunch = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchCamera(options, (res) => {
+      console.log('Response = ', res);
+      if (res.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (res.error) {
+        console.log('ImagePicker Error: ', res.error);
+      } else if (res.customButton) {
+        console.log('User tapped custom button: ', res.customButton);
+        alert(res.customButton);
+      } else {
+        const source = { uri: res.uri };
+        console.log('response', JSON.stringify(res));
+        this.setState({
+          filePath: res,
+          fileData: res.data,
+          fileUri: res.uri
+        });
+      }
+    });
+}
